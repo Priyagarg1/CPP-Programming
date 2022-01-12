4d65fbc0b6442c733e1262d061de622e166c4f32
priya garg <89633235+Priyagarg1@users.noreply.github.com>
2022-01-12 22:15:44 +0530

    Create Add.cpp

---
+++ Add.cpp
@@ -0,0 +1,16 @@
+//Code chef question 2
+#include<iostream>
+using namespace std;
+int main()
+{
+    int T,a,b,c,i;
+    cin>>T;
+    for(i=1;i<=T;i++)
+    {
+    cin>>a>>b;
+    c=a+b;
+    cout<<c;
+    cout<<"\n";
+    }
+    return 0;
+}

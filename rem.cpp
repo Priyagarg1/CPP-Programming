67c776355fb0cce8edce46b265e3219bada6e2d2
priya garg <89633235+Priyagarg1@users.noreply.github.com>
2022-01-16 22:55:08 +0530

    Create rem.cpp

---
+++ rem.cpp
@@ -0,0 +1,14 @@
+//codechef problem
+#include<iostream>
+using namespace std;
+int main(){
+    int T,a,b,c;
+    cin>>T;
+    while(T--)
+    {
+        cin>>a>>b;
+        c=a%b;
+        cout<<c<<endl;
+    }
+    return 0;
+}

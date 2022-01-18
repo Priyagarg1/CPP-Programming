c82d8f1f8cf56bfd8895716685ed970bdb377d69
priya garg <89633235+Priyagarg1@users.noreply.github.com>
2022-01-18 23:51:05 +0530

    Create find_sqr_root.cpp

---
+++ find_sqr_root.cpp
@@ -0,0 +1,17 @@
+//codechef problem
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+    int T,num,c;
+    cin>>T;
+    while(T--)
+    {
+        cin>>num;
+        c=sqrt(num);
+        cout<<c<<endl;
+    }
+	// your code goes here
+	return 0;
+}

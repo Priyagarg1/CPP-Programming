59f65d8d0b3d67be2806fe91a42d963f1506a6e6
priya garg <89633235+Priyagarg1@users.noreply.github.com>
2022-01-18 22:40:18 +0530

    Create helpingchef.cpp

---
+++ helpingchef.cpp
@@ -0,0 +1,22 @@
+//codechef problem
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T,num;
+    cin>>T;
+    while(T--)
+    {
+        cin>>num;
+        if(num<10)
+        {
+            cout<<"Thanks for helping Chef!"<<endl;
+        }
+        else
+        {
+            cout<<"-1"<<endl;
+        }
+    }
+	// your code goes here
+	return 0;
+}

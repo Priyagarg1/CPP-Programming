aefb30cd3db6d0782d6385208e640e24eb3dd4d1
priya garg <89633235+Priyagarg1@users.noreply.github.com>
2022-01-19 21:50:07 +0530

    Create Incre_decre.cpp

---
+++ Incre_decre.cpp
@@ -0,0 +1,20 @@
+// code chef problem
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cin>>num;
+    if(num%4==0)
+    {
+        num=num+1;
+        cout<<num<<endl;
+    }
+    else
+    {
+        num=num-1;
+        cout<<num<<endl;
+    }
+	// your code goes here
+	return 0;
+}

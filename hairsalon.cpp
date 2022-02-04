e0d4e9704ce0d92a662ddfc095def08cb6de0633
priya garg <89633235+Priyagarg1@users.noreply.github.com>
2022-02-04 22:02:50 +0530

    Create hairsalon.cpp

---
+++ hairsalon.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int N,M,total_weight;
+        cin>>N>>M;
+        total_weight=N*M;
+        cout<<total_weight<<endl;
+    }
+	// your code goes here
+	return 0;
+}

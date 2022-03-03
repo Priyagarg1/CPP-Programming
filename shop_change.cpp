36ebda8240e41a8f66deeb7bf8e2179868ff0deb
priya garg <89633235+Priyagarg1@users.noreply.github.com>
2022-03-03 19:47:15 +0530

    Create shop_change.cpp

---
+++ shop_change.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--)
+	{
+	    int x,shop_change;
+	    cin>>x;
+	    shop_change=100-x;
+	    cout<<shop_change<<endl;
+	    
+	}
+	return 0;
+}

1c3eddbafa74979106fa159b65226cdb7b98562a
priya garg <89633235+Priyagarg1@users.noreply.github.com>
2022-03-03 20:26:18 +0530

    Create notebook.cpp

---
+++ notebook.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int N,total_notebook;
+        cin>>N;
+        total_notebook=N*10;
+        cout<<total_notebook<<endl;
+    }
+	// your code goes here
+	return 0;
+}

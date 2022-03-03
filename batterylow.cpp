3be47ecac2de8f7e8cbd16d0e7e6b5ff0c868d0b
priya garg <89633235+Priyagarg1@users.noreply.github.com>
2022-03-03 19:42:14 +0530

    Create batterylow.cpp

---
+++ batterylow.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T,x;
+	cin>>T;
+	while(T--)
+	{
+	    cin>>x;
+	    
+	if(x<=15)
+	{
+	    cout<<"YES"<<endl;
+	}
+	else
+	{
+	    cout<<"NO"<<endl;
+	}
+	}
+	return 0;
+}
